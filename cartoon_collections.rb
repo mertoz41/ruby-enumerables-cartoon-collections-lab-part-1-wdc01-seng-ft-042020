4e95e32a1c8a5145ba911952e01787ae956f5551
Mert  Ozkaynak <mertozkaynak@windowslive.com>
2020-04-01 17:21:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,9 @@ end
   # Print a custom greeting for each element
 
 def list_dwarves(array)
-  # Use `each_with_index` to enumerate over the provided array
+  array.each_with_index do |character|
+end
+
+# Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
-end
